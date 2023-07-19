5e30be4e51e4991ca348308701cd9d1bdd2021fa
akhileshchamoli05 <chamoliakhilesh71@gmail.com>
2023-07-19 14:21:48 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Header from './components/Layout/Header'
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
-import {Fragment} from 'react';
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Header />
       <main>
         <Meals />
       </main>
